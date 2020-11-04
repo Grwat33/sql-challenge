ff8a7dec8bc7d1637797776b5f541aec6defe67a
Grwat33 <Grwat33@gmail.com>
2020-11-04 14:45:40 -0500

    first 3 questions

--- EmployeeSQL/pewletthackard_employee_info.sql
+++ EmployeeSQL/pewletthackard_employee_info.sql
@@ -60,3 +60,45 @@ REFERENCES "employees" ("emp_no");
 ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_dept_no" FOREIGN KEY("dept_no")
 REFERENCES "departments" ("dept_no");
 
+--List the following details of each employee: employee number, last name, first name, sex, and salary.
+SELECT
+	employees.emp_no,
+	employees.last_name,
+	employees.first_name,
+	employees.sex,
+	salaries.salary
+	FROM employees
+	LEFT JOIN salaries
+	ON employees.emp_no = salaries.emp_no
+	LIMIT 10
+
+--List first name, last name, and hire date for employees who were hired in 1986.
+SELECT
+	employees.first_name,
+	employees.last_name,
+	employees.hire_date
+	FROM employees
+	WHERE DATE_PART('year', hire_date) = 1986;
+
+--List the manager of each department with the following information: 
+--department number, department name, the manager's employee number, last name, first name.
+SELECT 
+	departments.dept_no, 
+	departments.dept_name, 
+	dept_manager.emp_no,
+	employees.last_name,
+	employees.first_name
+FROM departments
+INNER JOIN dept_manager ON departments.dept_no=dept_manager.dept_no
+LEFT OUTER JOIN employees ON dept_manager.emp_no=employees.emp_no
+	
+--List the department of each employee with the following information: employee number, last name, first name, and department name.
+
+--List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
+
+--List all employees in the Sales department, including their employee number, last name, first name, and department name.
+
+--List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
+
+--In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
+
